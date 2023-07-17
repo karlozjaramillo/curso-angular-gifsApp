8b51a4181e9ffc109c4c51ac5334b358184c7725
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2023-07-17 12:42:59 -0500

    @ViewChild - Obtener referencias a objetos del HTML

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
-  templateUrl: './busqueda.component.html'
+  templateUrl: './busqueda.component.html',
 })
 export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = '';
+  }
 }
