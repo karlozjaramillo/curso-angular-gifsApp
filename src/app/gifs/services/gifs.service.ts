1135ac2965afcab3a799438349b41ca56a6caf34
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2023-07-18 11:22:35 -0500

    Mostrar los resultados en pantalla

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -7,6 +7,7 @@ import { Injectable } from '@angular/core';
 export class GifsService {
   private apiKey: string = '7e2c3gr03IAJ5I5rDGdrC1a7JGCVc6mJ';
   private _historial: string[] = [];
+  public resultados: any[] = [];
 
   get historial() {
     return [...this._historial];
@@ -22,10 +23,11 @@ export class GifsService {
     }
     this.http
       .get(
-        'https://api.giphy.com/v1/gifs/search?api_key=7e2c3gr03IAJ5I5rDGdrC1a7JGCVc6mJ&q=dragon ball z&limit=10'
+        `https://api.giphy.com/v1/gifs/search?api_key=7e2c3gr03IAJ5I5rDGdrC1a7JGCVc6mJ&q=${query}&limit=10`
       )
       .subscribe((resp: any) => {
         console.log(resp.data);
+        this.resultados = resp.data;
       });
   }
 }
